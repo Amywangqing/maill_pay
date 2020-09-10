e7338c1fb9d71e2644fa39919e1aee5ade8bf74d
wq <wq@qq.com>
2020-09-10 14:56:47 +0800

    测试mysql连接

--- src/main/java/com/qingfeng/mall_pay/MallPayApplication.java
+++ src/main/java/com/qingfeng/mall_pay/MallPayApplication.java
@@ -1,9 +1,11 @@
 package com.qingfeng.mall_pay;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.qingfeng.mall_pay.dao"})//扫描dao层接口
 public class MallPayApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/qingfeng/mall_pay/MallPayApplicationTests.java
+++ src/test/java/com/qingfeng/mall_pay/MallPayApplicationTests.java
@@ -1,7 +1,10 @@
 package com.qingfeng.mall_pay;
 
+import com.qingfeng.mall_pay.dao.CategoryMapper;
+import com.qingfeng.mall_pay.pojo.Category;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MallPayApplicationTests {
 
+    @Autowired
+    private CategoryMapper categoryMapper;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void findById() {
+
+        Category category = categoryMapper.fingById(100001);
+        System.out.println(category);
     }
 
 }
